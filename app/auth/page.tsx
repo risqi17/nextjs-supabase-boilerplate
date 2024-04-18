f7d76257e902e2a53dad7e7a7e8dcb8064f17b31
risqiahmad <ahmadreys@gmail.com>
2024-04-18 22:59:23 +0700

    init commit

---
+++ app/auth/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React, { use } from 'react'
+import { Button } from "@/components/ui/button"
+import { KeyRound } from 'lucide-react'
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa6";
+import { supabaseBrowser } from '@/lib/supabase/browser';
+
+export default function page() {
+
+
+    const handleLoginWithOAuth = (provider: "github" | "google") => {
+		const supabase = supabaseBrowser();
+		supabase.auth.signInWithOAuth({
+			provider,
+			options: {
+				redirectTo: location.origin + "/auth/callback",
+			},
+		});
+	};
+
+  return (
+    <div className="flex items-center justify-center w-full h-screen">
+      <div className="w-96 rounded-md border p-5 space-y-5 relative bg-slate-900">
+        <div className="flex items-center gap-2">
+          <KeyRound />
+          <h1 className="text-2xl font-bold">Next + Supabase</h1>
+        </div>
+
+        <p className="text-sm text-gray-300">
+          Register/SignIn Today 👇
+        </p>
+        <div className="flex flex-col gap-5">
+          <Button onClick={() => handleLoginWithOAuth("github")} className="w-full flex items-center gap-2" variant="outline"><FaGithub /> Github</Button>
+          <Button onClick={() => handleLoginWithOAuth("google")} className="w-full flex items-center gap-2" variant="outline"><FcGoogle /> Google</Button>
+        </div>
+        <div className="glowBox -z-10"></div>
+      </div>
+    </div>
+  )
+}
